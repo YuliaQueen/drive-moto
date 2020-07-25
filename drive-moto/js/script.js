5bc3a176aa8ccc5a425992f8a6b9932c258ca699
Yulia <youlka25@yandex.ru>
2020-07-25 14:01:42 +0300

    верстка и стилизация секции search-section

--- drive-moto/js/script.js
+++ drive-moto/js/script.js
@@ -26,4 +26,15 @@ $(document).ready(function () {
         slidesToShow: 1,
         adaptiveHeight: true
     });
+
+    $('.search__tabs-item').on('click', function (e) {
+        e.preventDefault()
+
+        $('.search__tabs-item').removeClass('search__tabs-item--active');
+        $('.search__content-item').removeClass('search__content-item--active');
+
+        $(this).addClass('search__tabs-item--active');
+        $($(this).attr('href')).addClass('search__content-item--active');
+
+    })
 });

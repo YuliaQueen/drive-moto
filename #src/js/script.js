f589afb5574d33529089e9421c6dcece072c13cb
Yulia <youlka25@yandex.ru>
2020-07-26 16:35:18 +0300

    верстка и стилизация секции products-section

--- #src/js/script.js
+++ #src/js/script.js
@@ -17,6 +17,7 @@ testWebP(function (support) {
 });
 
 $(document).ready(function () {
+    //slick-slider для banner-section
     $('.banner-section__slider').slick({
         dots: true,
         prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="./img/arrow-left.svg" alt=""></button>',
@@ -27,14 +28,28 @@ $(document).ready(function () {
         adaptiveHeight: true
     });
 
-    $('.search__tabs-item').on('click', function (e) {
+    //slick-slider для products-section
+    // $('.product-slider').slick({
+    //     infinite: true,
+    //     slidesToShow: 4,
+    //     slidesToScroll: 1,
+    //     adaptiveHeight: true,
+    //     prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="./img/arrow-black-left.svg" alt=""></button>',
+    //     nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="./img/arrow-black-right.svg" alt=""></button>',
+    // });
+
+    //переключение табов
+    $('.tab').on('click', function (e) {
         e.preventDefault()
 
-        $('.search__tabs-item').removeClass('search__tabs-item--active');
-        $('.search__content-item').removeClass('search__content-item--active');
+        $($(this).siblings()).removeClass('tab--active');
+        $($(this).parent().siblings().find('div')).removeClass('tabs-content--active');
 
-        $(this).addClass('search__tabs-item--active');
-        $($(this).attr('href')).addClass('search__content-item--active');
-
-    })
+        $(this).addClass('tab--active');
+        $($(this).attr('href')).addClass('tabs-content--active');
+    });
+    //сердечки
+    $('.product-item-favorite').on('click', function () {
+        $(this).toggleClass('product-item-favorite--active')
+    });
 });
